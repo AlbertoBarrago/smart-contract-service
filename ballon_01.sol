eb3927670bd7096e5c9343e369c070dc8ff322e6
Alberto Barrago <albertobarrago@gmail.com>
2024-07-11 19:11:58 +0200

    Add first contract

---
+++ ballon_01.sol
@@ -0,0 +1,23 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract InBox {
+    string public message;
+    address public owner;
+
+    constructor() {
+        message = "";
+        owner = msg.sender;
+    }
+
+    function setMessage(string calldata newMessage) public {
+        message = newMessage;
+    }
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+
+    function getOwner() public view returns (address) {
+        return owner;
+    }
+}
