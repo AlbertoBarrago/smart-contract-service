213d7dceee2b07a1ab5945c74dee5fc4fcb91c7b
Alberto Barrago <albertobarrago@gmail.com>
2024-07-19 12:25:45 +0200

    Improved architecture and added Lottery contract

--- services/index.js
+++ services/index.js
@@ -1,31 +1,30 @@
-require('dotenv').config();
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-const {Web3} = require('web3');
-const {abi, evm} = require('./compile');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const {Web3} = require("web3");
 
+//Check if mnemonic and infura url are set in.env file
 if (!process.env.MNEMONIC || !process.env.INFURA_URL) {
     console.error('Please make sure MNEMONIC and INFURA_URL are set in your .env file');
     process.exit(1);
 }
-
+// Create a new HDWalletProvider instance with the mnemonic and infura url
 const provider = new HDWalletProvider(
     process.env.MNEMONIC,
     process.env.INFURA_URL
 );
 const web3 = new Web3(provider);
-
-const deploy = async () => {
+// Deploy the contract
+const deploy = async (contractAddress) => {
     try {
         const accounts = await web3.eth.getAccounts();
-        console.log('Attempting to deploy from account', accounts[0]);
+        console.log('Attempting to auth from account', accounts[0]);
 
         // Get the current nonce and gas price
         const nonce = await web3.eth.getTransactionCount(accounts[0], 'pending');
         const gasPrice = await web3.eth.getGasPrice();
-        const adjustedGasPrice = BigInt(gasPrice.toString()) * BigInt(120) / BigInt(100); // 120% of current gas price
+        const adjustedGasPrice = BigInt(gasPrice.toString()) * BigInt(120) / BigInt(100);
 
-        const contract = new web3.eth.Contract(abi);
-        const deployTransaction = contract.deploy({data: String(evm.bytecode.object), arguments: []});
+        const contract = new web3.eth.Contract(contractAddress.abi);
+        const deployTransaction = contract.deploy({data: contractAddress.bytecode, arguments: []});
 
         const gasLimit = await deployTransaction.estimateGas({from: accounts[0]});
 
@@ -47,10 +46,4 @@ const deploy = async () => {
     }
 };
 
-deploy().then((address) => {
-    console.log('Deployment successful. Contract address:', address);
-    process.exit(0);
-}).catch((err) => {
-    console.error('Deployment failed:', err);
-    process.exit(1);
-});
+module.exports = {web3, deploy};
--- test/authentication.test.js
+++ test/authentication.test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const ganache = require('ganache');
 const { Web3 } = require('web3');
 const web3 = new Web3(ganache.provider());
-const { abi , evm } = require('../compile');
+const { abi , evm } = require('../compiles/auth');
 
 let accounts;
 let authContract;
